8b26e0ccc1f8512391ffa5dcd38d7fcd7be9fb8f
dmiante <dmi4ndev@gmail.com>
2023-11-09 16:23:47 -0300

    ADD modules and routing

---
+++ src/app/countries/countries.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ByCapitalPageComponent } from './pages/by-capital-page/by-capital-page.component';
+
+
+
+@NgModule({
+  declarations: [
+    ByCapitalPageComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CountriesModule { }
