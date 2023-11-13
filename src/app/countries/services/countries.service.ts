b0f71e0e617f1c0044e350f02136c6974ea10a91
dmiante <dmi4ndev@gmail.com>
2023-11-13 20:57:28 -0300

    ADD searches by countries and region on pages

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
 import { Country } from '../interfaces/country';
+import { Observable, catchError, of } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class CountriesService {
@@ -9,7 +9,21 @@ export class CountriesService {
 
   constructor(private http: HttpClient) {}
 
-  searchCapital(query: string): Observable<Country[]>{
-    return this.http.get<Country[]>(`${this.apiURL}/capital/${query}`)
+  searchCapital(query: string): Observable<Country[]> {
+    return this.http
+      .get<Country[]>(`${this.apiURL}/capital/${query}`)
+      .pipe(catchError(() => of([])));
+  }
+
+  searchCountry(query: string): Observable<Country[]> {
+    return this.http
+      .get<Country[]>(`${this.apiURL}/name/${query}`)
+      .pipe(catchError(() => of([])));
+  }
+
+  searchRegion(region: string): Observable<Country[]> {
+    return this.http
+      .get<Country[]>(`${this.apiURL}/region/${region}`)
+      .pipe(catchError(() => of([])));
   }
 }
