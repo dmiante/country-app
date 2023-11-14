7d5e194f8c58058d57a00eeacd343369b4edcc9c
dmiante <dmi4ndev@gmail.com>
2023-11-13 21:56:13 -0300

    ADD countries details by code

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,13 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Country } from '../interfaces/country';
-import { Observable, catchError, of } from 'rxjs';
+import { Observable, catchError, map, of } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class CountriesService {
   private apiURL: string = 'https://restcountries.com/v3.1';
 
   constructor(private http: HttpClient) {}
+
+  searchCountryByAlphaCode(code: string): Observable<Country | null>{
+        return this.http
+          .get<Country[]>(`${this.apiURL}/alpha/${code}`)
+          .pipe(
+            map(countries => countries.length > 0 ? countries[0] : null),
+            catchError(() => of(null))
+            );
+  }
 
   searchCapital(query: string): Observable<Country[]> {
     return this.http
