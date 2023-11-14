bac828a0b014f8439b111ca30bb84e4cf50b25b5
dmiante <dmi4ndev@gmail.com>
2023-11-14 16:05:04 -0300

    FIX search region for static button

--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,13 +1,14 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { Subject, debounceTime } from 'rxjs';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { Subject, Subscription, debounceTime } from 'rxjs';
 
 @Component({
   selector: 'shared-search-box',
   templateUrl: './search-box.component.html',
   styleUrls: ['./search-box.component.css'],
 })
-export class SearchBoxComponent implements OnInit {
+export class SearchBoxComponent implements OnInit, OnDestroy {
   private debouncer: Subject<string> = new Subject<string>();
+  private debouncerSuscription?: Subscription;
 
   @Input()
   public placeholder: string = '';
@@ -19,9 +20,14 @@ export class SearchBoxComponent implements OnInit {
   public onDebounce = new EventEmitter<string>();
 
   ngOnInit(): void {
+    this.debouncerSuscription =
     this.debouncer.pipe(debounceTime(300)).subscribe((value) => {
       this.onDebounce.emit(value)
     });
+  }
+
+  ngOnDestroy(): void {
+    this.debouncerSuscription?.unsubscribe()
   }
 
   emitValue(value: string): void {
